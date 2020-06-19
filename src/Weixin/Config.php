3a2aff092bb164796254d63d8ed64e0fe5de5cf4
高彬展 <gaobinzhan@gmail.com>
2020-06-19 15:36:24 +0800

    add weixin oauth login

---
+++ src/Weixin/Config.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * @author gaobinzhan <gaobinzhan@gmail.com>
+ */
+
+
+namespace EasySwoole\OAuth\Weixin;
+
+
+class Config
+{
+    protected $appId = '';
+
+    protected $secret = '';
+
+    protected $redirectUri = '';
+
+    protected $responseType = 'code';
+
+    protected $scope = 'snsapi_login';
+
+    protected $state = '';
+
+    protected $grantType = 'authorization_code';
+
+    protected $lang = 'zh_CN';
+
+    /**
+     * @return string
+     */
+    public function getLang(): string
+    {
+        return $this->lang;
+    }
+
+    /**
+     * @param string $lang
+     */
+    public function setLang(string $lang): void
+    {
+        $this->lang = $lang;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAppId(): string
+    {
+        return $this->appId;
+    }
+
+    /**
+     * @param string $appId
+     */
+    public function setAppId(string $appId): void
+    {
+        $this->appId = $appId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecret(): string
+    {
+        return $this->secret;
+    }
+
+    /**
+     * @param string $secret
+     */
+    public function setSecret(string $secret): void
+    {
+        $this->secret = $secret;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRedirectUri(): string
+    {
+        return $this->redirectUri;
+    }
+
+    /**
+     * @param string $redirectUri
+     */
+    public function setRedirectUri(string $redirectUri): void
+    {
+        $this->redirectUri = $redirectUri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResponseType(): string
+    {
+        return $this->responseType;
+    }
+
+    /**
+     * @param string $responseType
+     */
+    public function setResponseType(string $responseType): void
+    {
+        $this->responseType = $responseType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScope(): string
+    {
+        return $this->scope;
+    }
+
+    /**
+     * @param string $scope
+     */
+    public function setScope(string $scope): void
+    {
+        $this->scope = $scope;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState(string $state): void
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGrantType(): string
+    {
+        return $this->grantType;
+    }
+
+    /**
+     * @param string $grantType
+     */
+    public function setGrantType(string $grantType): void
+    {
+        $this->grantType = $grantType;
+    }
+}
---
+++ src/Weixin/OAuth.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @author gaobinzhan <gaobinzhan@gmail.com>
+ */
+
+
+namespace EasySwoole\OAuth\Weixin;
+
+
+use EasySwoole\HttpClient\HttpClient;
+use EasySwoole\OAuth\BaseOAuth;
+use EasySwoole\OAuth\OAuthException;
+
+class OAuth extends BaseOAuth
+{
+    const API_DOMAIN = 'https://api.weixin.qq.com';
+
+    const OPEN_DOMAIN = 'https://open.weixin.qq.com';
+
+    /** @var Config */
+    protected $config;
+
+    protected $openId;
+
+    public function getAuthUrl()
+    {
+        $params = [
+            'appid' => $this->config->getAppId(),
+            'redirect_uri' => $this->config->getRedirectUri(),
+            'response_type' => $this->config->getResponseType(),
+            'scope' => $this->config->getScope(),
+            'state' => $this->config->getState(),
+        ];
+        return $this->getUrl(self::OPEN_DOMAIN . '/connect/qrconnect', $params) . '#wechat_redirect';
+    }
+
+    protected function __getAccessToken($state = null, $code = null)
+    {
+        $client = (new HttpClient(self::API_DOMAIN . '/sns/oauth2/access_token'))
+            ->setQuery([
+                'appid' => $this->config->getAppId(),
+                'secret' => $this->config->getSecret(),
+                'code' => $code,
+                'grant_type' => $this->config->getGrantType(),
+            ])->get();
+
+        $body = $client->getBody();
+        if (!$body) throw new OAuthException('获取AccessToken失败！');
+
+        $result = \json_decode($body, true);
+
+        if (isset($result['errcode']) && 0 != $result['errcode']) {
+            throw new OAuthException($result['errmsg'], $result['errcode']);
+        }
+
+        $this->openId = $result['openid'];
+        return $result['access_token'];
+    }
+
+    public function getUserInfo(string $accessToken)
+    {
+        $client = (new HttpClient(self::API_DOMAIN . '/sns/userinfo'))
+            ->setQuery([
+                'access_token' => $accessToken,
+                'openid' => $this->openId,
+                'lang' => $this->config->getLang(),
+            ])
+            ->get();
+
+        $body = $client->getBody();
+        if (!$body) throw new OAuthException('获取用户信息失败！');
+
+        $result = \json_decode($body, true);
+
+        if (isset($result['errcode']) && 0 != $result['errcode']) {
+            throw new OAuthException($result['errmsg'], $result['errcode']);
+        }
+
+        return $result;
+    }
+}
